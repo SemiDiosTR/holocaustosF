550e34337ace5c32eea086e79e32d0ba98d7bdbf
SemiDios <hugoalbertomik@gmail.com>
2025-04-10 13:20:59 -0600

    Coloritos para valor de ESTADO en Pedido

--- src/app/components/pedidos/pedidos.component.ts
+++ src/app/components/pedidos/pedidos.component.ts
@@ -36,6 +36,7 @@ export class PedidosComponent {
 
 ngOnInit(): void{
   this.loadPedidos();
+   
 }
 
 loadPedidos():void{
@@ -56,12 +57,74 @@ toggleForm(): void{
 }
 
 /* ESTATUS STYLE */
-estadoStyle(): void{
-  this.showForm = !this.showForm;
-  this.textoModal = "Nuevo pedido";
-  this.isEditMode = false;
-  this.selectedPedido = null;
-  this.pedidosForm.reset();
+estadoColor: String = 'text-warning';
+estadoStyle( estado: string): String{
+  this.estadoColor = '';
+  
+  if(estado == '0'  ){
+    this.estadoColor = 'text-danger'; 
+  }
+
+  if(estado == '1'  ){
+    this.estadoColor = 'text-primary'; 
+  }
+  
+  if(estado == '2'  ){
+    this.estadoColor = 'text-warning';
+  }
+
+  if(estado == '3'  ){
+    this.estadoColor = 'text-success';
+  }
+  return this.estadoColor;
+}
+
+/*ESTATUS TO STRING
+estadoText : string = 'unchanged';
+estadoToString (estado : string): string{
+
+ 
+  switch(estado) {
+    case '0' : {
+      this.estadoText='CANCELADO';
+      return this.estadoText;
+      break;
+    }
+
+    case '1' : {
+      this.estadoText = 'PENDIENTE';
+      return this.estadoText;
+    }
+
+    case '2' : {
+      this.estadoText = 'ENVIADO';
+      return this.estadoText;
+    }
+
+    case '3' : {
+      this.estadoText = 'ENTREGADO';
+      return this.estadoText;
+    }
+
+    default: {
+      this.estadoText= 'INDEFINIDO';
+      return this.estadoText;
+    }
+  }
+}
+*/
+
+estadoText : string = '';
+estadoToString(estado: string): string {
+  const mapping: { [key: string]: string } = {
+      '0': 'CANCELADO',
+      '1': 'PENDIENTE',
+      '2': 'ENVIADO',
+      '3': 'ENTREGADO',
+  };
+
+  this.estadoText = mapping[estado] || 'INDEFINIDO';
+  return this.estadoText;
 }
 
 crearPedidos(): void{
